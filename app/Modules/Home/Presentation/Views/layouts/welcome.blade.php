6a406c1b73e17cb84a7b4d5584c8a3e215dd07fd
Lars Wiegers <larswiegers@live.nl>
2018-12-06 12:04:04 +0100

    update seo

--- app/Modules/Home/Presentation/Views/layouts/welcome.blade.php
+++ app/Modules/Home/Presentation/Views/layouts/welcome.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Lars Wiegers Homepage">
+        <link rel="icon" type="image/png" href="{{asset('icons/favicon/favicon-32x32.png')}}" sizes="32x32" />
+        <link rel="icon" type="image/png" href="{{asset('icons/favicon/favicon-16x16.png')}}" sizes="16x16" />
 
         <title>Lars Wiegers Blog</title>
 
--- app/Modules/Home/Presentation/Views/pages/home.blade.php
+++ app/Modules/Home/Presentation/Views/pages/home.blade.php
@@ -13,7 +13,7 @@
                 </ul>
             </div>
             <div class="image">
-                <img src="{{asset('images/home-images/banner.jpg')}}" alt="" />
+                <img src="{{asset('images/home-images/banner.jpg')}}" alt="Person coding" />
             </div>
         </section>
 
@@ -24,7 +24,7 @@
                 <p>Ik ben een beginnend applicatieontwikkelaar in Apeldoorn die zich vooral richt op het de hele web development stack. Ik ben op dit moment bezig mij verder te ontwikkelen op dit gebied door een hbo opleiding te doen. Tijdens mijn vorige opleiding heb ik de mogelijkheid gekregen om mijn school te vertegenwoordigen op de Nederlandse wedstrijden van het mbo. Bij deze wedstrijden ben ik <b>4e van Nederland</b> geworden.</p>
             </div>
             <div class="image">
-                <img src="{{asset('images/home-images/lars.jpg')}}" alt="" />
+                <img src="{{asset('images/home-images/lars.jpg')}}" alt="Picture of me" />
             </div>
         </section>
 
@@ -35,7 +35,7 @@
                 <p>Het grootste gedeelte van mijn software projecten zijn te vinden op mijn  <a href="https://github.com/LarsWiegers">github</a>. Daarnaast ben ik ook nog actief op <a href="https://linkedin.com/in/larswiegers">linkedin</a>. Daarnaast kunt u mij ook nog mailen als u in contact wil komen : <a href="mailto:larswiegers@live.nl">larswiegers@live.nl</a></p>
             </div>
             <div class="image">
-                <img src="{{asset('images/home-images/pic03.png')}}" alt="" />
+                <img src="{{asset('images/home-images/pic03.png')}}" alt="Picture of my code" />
             </div>
         </section>
         <!-- Footer -->
