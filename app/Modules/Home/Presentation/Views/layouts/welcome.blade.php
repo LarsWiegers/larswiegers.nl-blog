5a52727d32ca65e4473ac2b695057d9de8f9840d
Lars Wiegers <larswiegers@live.nl>
2019-06-26 15:14:11 +0200

    Should choose the right file to update

--- app/Modules/Home/Presentation/Views/layouts/welcome.blade.php
+++ app/Modules/Home/Presentation/Views/layouts/welcome.blade.php
@@ -14,6 +14,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{asset('css/home-main.css')}}" />
+        <link rel="stylesheet" href="{{asset('manifest.json')}}" />
     </head>
     <body>
         @yield("body")
